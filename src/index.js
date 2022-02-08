374504f5d37a9cf0dae7caef62bdd6db8d29768e
NAZIRwill29 <59091291+NAZIRwill29@users.noreply.github.com>
2022-02-08 20:30:33 +0800

    second add two js in h1 add expression

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Nazir";
+const fname = "Mohamad";
+const lname = "Nazir";
 
 const luckyNumber = Math.round(Math.random() * 100);
 
 //{} make it regard as javascript
 ReactDOM.render(
   <div>
-    <h1>Hello {name}!</h1>
+    <h1>
+      Hello {fname} {lname}!
+    </h1>
+    <p>your lucky number is {3 + 4} </p>
     <p>your lucky number is {luckyNumber} </p>
   </div>,
   document.getElementById("root")
